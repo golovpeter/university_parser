94748f8322c3adcad8fae442920c0495828a204a
golovpeter <petya-golov@mail.ru>
2021-07-20 17:23:45 +0300

    delete TOKEN

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 import telebot
 
-TOKEN = '1887025124:AAGvIxRPEmIrsSVjRHuNxilDYKFL3w0gQLg'
-BOT_URL = 'https://university-contest-parser-bot.herokuapp.com/'
+TOKEN = ''
 
 bot = telebot.TeleBot(TOKEN)
