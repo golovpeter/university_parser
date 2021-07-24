e53cb4f680e5b161dba6b4b7705e78c18b6f68f2
golovpeter <petya-golov@mail.ru>
2021-07-24 20:18:38 +0300

    delete local token

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 import telebot
 
-TOKEN = os.environ.get('BOT_TOKEN', '1176140428:AAHBGNoRYcdFKEUhLGg7eO7dYF-6B-8ZaS8')
+TOKEN = os.environ.get('BOT_TOKEN')
 APP_URL = 'https://university-contest-parser.herokuapp.com/'
 SNILS_MIREA = os.environ.get('SNILS_MIREA')
 SNILS = os.environ.get('SNILS')
