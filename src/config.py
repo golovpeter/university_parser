045b640df0245acfdab5daf9721e5dc4235c0785
golovpeter <petya-golov@mail.ru>
2021-07-24 20:14:33 +0300

    change TOKEN

--- src/config.py
+++ src/config.py
@@ -2,8 +2,7 @@ import os
 
 import telebot
 
-# TOKEN = os.environ.get('BOT_TOKEN')
-TOKEN = "1887025124:AAGvIxRPEmIrsSVjRHuNxilDYKFL3w0gQLg"
+TOKEN = os.environ.get('BOT_TOKEN', '1176140428:AAHBGNoRYcdFKEUhLGg7eO7dYF-6B-8ZaS8')
 APP_URL = 'https://university-contest-parser.herokuapp.com/'
 SNILS_MIREA = os.environ.get('SNILS_MIREA')
 SNILS = os.environ.get('SNILS')
