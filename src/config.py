cd1b06eaf55af4f470d54f532a2b1193bf5e2271
golovpeter <petya-golov@mail.ru>
2021-07-26 19:11:29 +0300

    refactoring

--- src/config.py
+++ src/config.py
@@ -2,9 +2,11 @@ import os
 
 import telebot
 
-TOKEN = os.environ.get('BOT_TOKEN')
 APP_URL = 'https://university-contest-parser.herokuapp.com/'
+
+TOKEN = os.environ.get('BOT_TOKEN')
 SNILS_MIREA = os.environ.get('SNILS_MIREA')
 SNILS = os.environ.get('SNILS')
+MGSU_NUM = os.environ.get('MGSU_NUM')
 
 bot = telebot.TeleBot(TOKEN)
