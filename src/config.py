edb9d57ae3f4e8559f28033876d4e564258a3da1
golovpeter <petya-golov@mail.ru>
2021-07-27 18:38:16 +0300

    fix bug

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import telebot
 
 APP_URL = 'https://university-contest-parser.herokuapp.com/'
 
-TOKEN = '1176140428:AAHBGNoRYcdFKEUhLGg7eO7dYF-6B-8ZaS8'
+TOKEN = os.environ.get('BOT_TOKEN')
 SNILS_MIREA = os.environ.get('SNILS_MIREA')
 SNILS = os.environ.get('SNILS')
 MGSU_NUM = os.environ.get('MGSU_NUM')
