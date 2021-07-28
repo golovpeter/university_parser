a96f0f2c0221ca08324e8ce424a9bc1042e0809a
golovpeter <petya-golov@mail.ru>
2021-07-28 23:24:10 +0300

    fix bug

--- src/handlers/text_handler.py
+++ src/handlers/text_handler.py
@@ -25,4 +25,4 @@ def handle_university(message):
     if message.text == UNIVERSITIES[2]:
         bot.send_message(message.chat.id,
                          mgsu_create_string(MGSU_FACULTIES, MGSU_BUDGET_PLACES, mgsu_parser(MGSU_URL)[0],
-                                            mgsu_parser(MGSU_URL)[1], mgsu_parser(MGSU_URL)[2]))
+                                            mgsu_parser(MGSU_URL)[1]))
